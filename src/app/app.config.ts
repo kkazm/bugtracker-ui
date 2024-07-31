9b5dac27f1f8cc724b056f09e14b0531e9d49c4c
Konrad <89358750+kkazm@users.noreply.github.com>
2024-07-31 12:47:31 +0200

    @angular/core migration - migration-http-providers
    Replace deprecated HTTP related modules with provider functions

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { authenticationInterceptor } from './authentication.interceptor';
 import { provideDateFnsAdapter } from '@angular/material-date-fns-adapter';
