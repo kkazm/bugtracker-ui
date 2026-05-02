7dcf9f12c21bdb07bef98c046eee137d182ec38a
Konrad <89358750+kkazm@users.noreply.github.com>
2026-05-02 20:49:05 +0200

    build: Add less preprocessor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {Main} from './main/main';
 import {SpringIssues} from './spring-issues/spring-issues';
+import TaigaDash from './taiga-dash/taiga-dash';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'spring-issues',
     component: SpringIssues
+  },
+  {
+    path: 'taiga',
+    component: TaigaDash
   }
 ];
